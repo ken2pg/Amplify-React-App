dd57b24985dddf25d9c31a317e3577ca0b9ad257
kento <jst34zais9102z@gmail.com>
2021-02-06 19:01:10 +0900

    認証画面の追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Amplify from 'aws-amplify';
+import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import awsmobile from './aws-exports';
+
+Amplify.configure(awsmobile);
 
 const App = () => {
-  return (
+  const [authState, setAuthState] = useState<AuthState>();
+  const [user, setUser] = useState<object | undefined>();
+  useEffect(() => {
+    onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData);
+    });
+  }, []);
+  return authState === AuthState.SignedIn && user ? (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AmplifySignOut />
     </div>
+  ) : (
+    <AmplifyAuthenticator />
   );
 };
 
