ede285e8a4fff629b0e630fd2aeec3b3b5e498aa
kento <jst34zais9102z@gmail.com>
2021-03-04 20:34:07 +0900

    React Contextの追加、Graphqlの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
-import React, { useEffect, useState } from 'react';
+//React,css関連ファイル
+import React, { useEffect, useState, createContext } from 'react';
 import './App.css';
-import Amplify from 'aws-amplify';
+
+//Amplify関連のライブラリ
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
 import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
 import awsmobile from './aws-exports';
+import { listTodos } from './graphql/queries';
+import { GraphQLResult } from '@aws-amplify/api/lib/types';
+import { ListTodosQuery } from './API';
+
+//component関連ファイル
+import InputForm from './component/input';
 
 Amplify.configure(awsmobile);
+
+type Todo = {
+  id: string;
+  name: string;
+  description: string;
+  completed: boolean;
+};
+
+//ReactContext定義
+export const todoListContext = createContext(
+  {} as {
+    todoList: Todo[];
+    setTodoList: React.Dispatch<React.SetStateAction<Todo[]>>;
+  }
+);
 
 const App = () => {
   const [authState, setAuthState] = useState<AuthState>();
   const [user, setUser] = useState<object | undefined>();
+
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+
+  const fetchTodo = async () => {
+    const res = (await API.graphql(graphqlOperation(listTodos))) as GraphQLResult;
+    const todo = res.data as ListTodosQuery;
+    if (todo.listTodos != null) {
+      setTodoList(todo.listTodos.items as Todo[]);
+    }
+    await console.log('success');
+  };
+
   useEffect(() => {
     onAuthUIStateChange((nextAuthState, authData) => {
       setAuthState(nextAuthState);
       setUser(authData);
     });
+    fetchTodo();
   }, []);
   return authState === AuthState.SignedIn && user ? (
     <div className="App">
-      <AmplifySignOut />
+      <todoListContext.Provider value={{ todoList, setTodoList }}>
+        <h2>todoList</h2>
+        <InputForm />
+        <AmplifySignOut />
+      </todoListContext.Provider>
     </div>
   ) : (
     <AmplifyAuthenticator />
